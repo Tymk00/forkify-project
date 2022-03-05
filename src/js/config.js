a0ff63dfb7016aafbbe28995cb4d110b5ec78890
Tymoteusz Jaskulski <100036483+Tymk00@users.noreply.github.com>
2022-03-05 17:42:28 +0100

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9372f934-edeb-4f77-89ee-04c61f486a30';
+export const MODAL_CLOSE_SEC = 2.5;
